66943faaf7211836be7222158604c02b8945b0ae
ryan <112927924+mamasbickboi@users.noreply.github.com>
2026-01-30 11:49:00 +0000

    Create smt.cpp

---
+++ smt.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <print>
+
+void permutations (std::string& str, uint8_t start, uint8_t end)
+{
+    if ()
+    {
+        std::println("str");
+        return;
+    }
+
+    //
+    //
+    //
+}
+
+int main ()
+{
+    std::string x = "abc";
+
+    permutations(x, 0, 2);
+
+    std::cin.get();
+}
